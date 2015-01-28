672eb4e14a9ae199eebcf44a7e457770031cbef9
unknown <mschout@gkg.net>
2015-01-28 01:09:47 -0600

    import Crypt-OpenSSL-EC 0.8
    git-cpan-module:   Crypt-OpenSSL-EC
    git-cpan-version:  0.8
    git-cpan-authorid: unknown@cpan.org

--- lib/Crypt/OpenSSL/EC.pm
+++ lib/Crypt/OpenSSL/EC.pm
@@ -375,7 +375,7 @@ our @EXPORT = qw(
 	POINT_CONVERSION_UNCOMPRESSED
 );
 
-our $VERSION = '0.7';
+our $VERSION = '0.8';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
